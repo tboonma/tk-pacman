fb2d06acc10fd6e143856aa3cdb8a133f1895691
tboonma <tboonma@hotmail.com>
2021-03-25 11:42:31 +0700

    Scoring update

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ class Pacman(Sprite):
         self.c = c
         self.maze = maze
 
+        self.dot_eaten_observers = []
+
         self.direction = DIR_STILL
         self.next_direction = DIR_STILL
 
@@ -30,6 +32,8 @@ class Pacman(Sprite):
 
             if self.maze.has_dot_at(r, c):
                 self.maze.eat_dot_at(r, c)
+                for i in self.dot_eaten_observers:
+                    i()
             
             if self.maze.is_movable_direction(r, c, self.next_direction):
                 self.direction = self.next_direction
@@ -56,11 +60,29 @@ class PacmanGame(GameApp):
         self.elements.append(self.pacman1)
         self.elements.append(self.pacman2)
 
+        self.pacman1_score = 0
+        self.pacman2_score = 0
+
+        self.pacman1.dot_eaten_observers.append(self.dot_eaten_by_pacman1)
+        self.pacman2.dot_eaten_observers.append(self.dot_eaten_by_pacman2)
+
     def pre_update(self):
         pass
 
     def post_update(self):
         pass
+
+    def update_scores(self):
+        self.pacman1_score_text.set_text(f'P1: {self.pacman1_score}')
+        self.pacman2_score_text.set_text(f'P2: {self.pacman2_score}')
+    
+    def dot_eaten_by_pacman1(self):
+        self.pacman1_score += 1
+        self.update_scores()
+
+    def dot_eaten_by_pacman2(self):
+        self.pacman2_score += 1
+        self.update_scores()
 
     def on_key_pressed(self, event):
         if event.char.upper() == 'A':
