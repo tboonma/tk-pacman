4c7f1da403fc44a5206cd53039df28ba3b92a9e8
tboonma <tboonma@hotmail.com>
2021-03-25 13:30:50 +0700

    Flyweight pattern

--- gamelib.py
+++ gamelib.py
@@ -50,12 +50,16 @@ class Text(GameCanvasElement):
         
 
 class Sprite(GameCanvasElement):
-    def __init__(self, game_app, image_filename, x=0, y=0):
+    def __init__(self, game_app, image_filename, x=0, y=0, photo_image=None):
         self.image_filename = image_filename
+
+        self.photo_image = photo_image
+
         super().__init__(game_app, x, y)
 
     def init_canvas_object(self):
-        self.photo_image = tk.PhotoImage(file=self.image_filename)
+        if not self.photo_image:
+            self.photo_image = tk.PhotoImage(file=self.image_filename)
         self.canvas_object_id = self.canvas.create_image(
             self.x, 
             self.y,
--- maze.py
+++ maze.py
@@ -1,10 +1,11 @@
 from gamelib import Sprite
 from dir_consts import *
+import tkinter as tk
 
 class Dot(Sprite):
-    def __init__(self, app, x, y):
-        super().__init__(app, 'images/dot.png', x, y)
-
+    def __init__(self, app, x, y, photo_image=None):
+        super().__init__(app, 'images/dot.png', x, y, photo_image=photo_image)
+        
         self.is_eaten = False
 
     def get_eaten(self):
@@ -13,8 +14,8 @@ class Dot(Sprite):
 
 
 class Wall(Sprite):
-    def __init__(self, app, x, y):
-        super().__init__(app, 'images/wall.png', x, y)
+    def __init__(self, app, x, y, photo_image=None):
+        super().__init__(app, 'images/wall.png', x, y, photo_image=photo_image)
 
 
 class Maze:
@@ -58,16 +59,19 @@ class Maze:
 
         self.init_active_dots()
 
+        self.wall_image = tk.PhotoImage(file='images/wall.png')
+        self.dot_image = tk.PhotoImage(file='images/dot.png')
+
         for i in range(self.get_height()):
             for j in range(self.get_width()):
                 x, y = self.piece_center(i, j)
 
                 if self.has_wall_at(i, j):
-                    wall = Wall(self.app, x, y)
+                    wall = Wall(self.app, x, y, photo_image=self.wall_image)
                     self.walls.append(wall)
 
                 if self.has_dot_at(i, j):
-                    dot = Dot(self.app, x, y)
+                    dot = Dot(self.app, x, y, photo_image=self.dot_image)
                     self.dots[(i,j)] = dot
 
     def __init__(self, app, canvas_width, canvas_height):
